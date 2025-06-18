8b6c9af07bdd01675450a3116648ac1c744f1245
Phadon Makara <phadonmakara@gmail.com>
2025-06-19 00:53:14 +0700

    carousel now use react-slick library

--- src/Carousel.jsx
+++ src/Carousel.jsx
@@ -1,24 +1,33 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { shortList, list, longList } from './data'
 import { FaQuoteRight } from 'react-icons/fa'
 import { FiChevronLeft, FiChevronRight } from 'react-icons/fi'
 
 function Carousel() {
   const [people, setPeople] = useState(list)
-  const [currentPerson, settCurrentPerson] = useState(0)
+  const [currentPerson, setCurrentPerson] = useState(0)
 
   const prevSlide = () => {
-    settCurrentPerson((current) => {
+    setCurrentPerson((current) => {
       const result = (current - 1 + people.length) % people.length
       return result
     })
   }
   const nextSlide = () => {
-    settCurrentPerson((current) => {
+    setCurrentPerson((current) => {
       const result = (current + 1) % people.length
       return result
     })
   }
+
+  useEffect(() => {
+    let sliderId = setInterval(() => {
+      nextSlide()
+    }, 2000)
+    return () => {
+      clearInterval(sliderId)
+    }
+  }, [currentPerson])
 
   return (
     <section className='slider-container'>
@@ -29,6 +38,8 @@ function Carousel() {
             className='slide'
             style={{
               transform: `translateX(${100 * (personIndex - currentPerson)}%)`,
+              opacity: personIndex === currentPerson ? 1 : 0,
+              visibility: personIndex === currentPerson ? 'visible' : 'hidden',
             }}
             key={id}
           >
